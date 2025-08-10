5715c739963ad6946725da24752231760567ca5c
jessmahendra <63407931+jessmahendra@users.noreply.github.com>
2025-08-10 19:14:44 +0100

    plpausbile package

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import PlausibleProvider from "next-plausible";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -28,7 +29,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <PlausibleProvider domain="pointfour.in">{children}</PlausibleProvider>
       </body>
     </html>
   );
